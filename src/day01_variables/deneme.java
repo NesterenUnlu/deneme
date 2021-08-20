f1ad11b12606300a93976bec80d61ff66c2d5d28
Nesteren <alyaarda99@gmail.com>
2021-08-20 17:03:35 -0400

    deneme

---
+++ src/day01_variables/deneme.java
@@ -0,0 +1,13 @@
+package day01_variables;
+
+public class deneme {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world");
+
+        System.out.println("Bu son tekrardi");
+
+    }
+
+}
